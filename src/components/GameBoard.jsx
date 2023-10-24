d4947ce211db252ca2bf49a3fe4d4563d91e1805
Dobuss <dobromira.ivanova911@gmail.com>
2023-10-24 16:41:17 +0300

    implementing switching between players functionality

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ]
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
+   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+   function handleSelectSquare(rowIndex, colIndex){
+    setGameBoard((prevGameBoard) => {
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+        return updatedBoard; // update a copy of the state when it is object or array
+    });
+
+    onSelectSquare();
+
+   }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+      {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
             {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
             </li>)}
         </ol>
       </li>)}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
 const [name, setName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -19,7 +19,7 @@ const [name, setName] = useState(initialName);
   }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
