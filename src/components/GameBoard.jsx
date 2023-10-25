a5dbdad4bd0be6d2ecd35017ec2dbaa1cb73cbcd
Dobuss <dobromira.ivanova911@gmail.com>
2023-10-25 12:53:42 +0300

    implementation of winning func

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,34 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  //    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //    function handleSelectSquare(rowIndex, colIndex){
-  //     setGameBoard((prevGameBoard) => {
-  //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-  //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //         return updatedBoard; // update a copy of the state when it is object or array
-  //     });
-
-  //     onSelectSquare();
-
-  //    }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
