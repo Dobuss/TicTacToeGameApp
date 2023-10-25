58cb01157dcf28cc8a523179c8057c195c1bd520
Dobuss <dobromira.ivanova911@gmail.com>
2023-10-25 15:00:20 +0300

    game over implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 const PLAYERS = {
   X: 'Player 1',
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
-const [name, setName] = useState(initialName);
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [name, setName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     //setIsEditing(!isEditing); //this syntax inverts the value
     setIsEditing((prevState) => !prevState); // best way to update state based on prev state
+    if (isEditing) {
+      onChangeName(symbol, name);
+    }
   }
 
-  function handleChange(e){
+  function handleChange(e) {
     setName(e.target.value);
   }
 
   let playerName = <span className="player-name">{name}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name} onChange={handleChange}></input>;
+    playerName = (
+      <input type="text" required value={name} onChange={handleChange}></input>
+    );
   }
 
   return (
-    <li className={isActive ? 'active' : undefined}>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
